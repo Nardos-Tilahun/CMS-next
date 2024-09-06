ea57ca50dfb4d9f45950dfd8d8e6b9f720ba6e33
Nardos Tilahun <contactnardos@gmail.com>
2024-09-06 18:32:13 +0300

    authentication

--- auth.ts
+++ auth.ts
@@ -7,31 +7,38 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     Google,
     Credentials({
       credentials: {
-        username: { label: 'Username', type: 'text' },
-        password: { label: 'Password', type: 'password' },
+        username: { label: 'Username', type: 'text', placeholder: 'Email' },
+        password: {
+          label: 'Password',
+          type: 'password',
+          placeholder: 'Password',
+        },
       },
       async authorize(credentials) {
-        if (!credentials?.username || !credentials?.password) {
-          return null;
-        }
-
+        let user = null;
+        // if (!credentials?.username || !credentials?.password) {
+        //   return null;
+        // }
         // Replace this with your actual API call logic
-        const response = await fetch('your-api-endpoint', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({
-            username: credentials.username,
-            password: credentials.password,
-          }),
-        });
-
-        if (!response.ok) {
-          return null;
-        }
-
-        const user = await response.json();
+        // const response = await fetch('your-api-endpoint', {
+        //   method: 'POST',
+        //   headers: {
+        //     'Content-Type': 'application/json',
+        //   },
+        //   body: JSON.stringify({
+        //     username: credentials.username,
+        //     password: credentials.password,
+        //   }),
+        // });
+        // if (!response.ok) {
+        //   return null;
+        // }
+        // const user = await response.json();
+        user = {
+          id: 1,
+          name: 'John Doe',
+          email: 'john@email.com',
+        };
         if (!user) {
           return null;
         }
@@ -41,3 +48,11 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     }),
   ],
 });
+/**
+ * If the user object is null or undefined, we return null, which will
+ * cause NextAuth to not log the user in.
+ */
+/**
+ * If the login is successful, return the user object. This will cause
+ * NextAuth to log the user in.
+ */
